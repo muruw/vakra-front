8fc0f9e64e4d3c0a879e52599477fbe6b5564d90
Marcus Murumäe <marcus@snowhound.eu>
2020-07-24 17:52:17 +0300

    #2 replace datepicker dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
 import { BookingShort } from './booking/short/BookingShort';
-import { BookingCreation } from './booking/BookingCreation';
 
 function App() {
   return (
     <div>
-      <BookingShort bookingCreation={new BookingCreation()}/>
+      <BookingShort/>
     </div>
   );
 }
